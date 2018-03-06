680ac17e704497f9dd7434520f1052a9239daa40
aleksei <alekseiosh@gmail.ru>
2018-03-06 21:25:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -0,0 +1,28 @@
+
+package com.example.android.fragmentexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass to display
+ * radio buttons.
+ */
+public class SimpleFragment extends Fragment {
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return null;
+    }
+}
